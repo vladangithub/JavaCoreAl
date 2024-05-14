39391c66ef6d33385c640b1105c081e66518239b
azesm <azesm@yahoo.com>
2024-05-14 11:19:52 +0300

    Less16

--- src/JavaBeginners21/ClassesAndObjects.java
+++ src/JavaBeginners21/ClassesAndObjects.java
@@ -4,7 +4,7 @@ public class ClassesAndObjects {
     public static void main(String[] args) {
         Person person1 = new Person();
         person1.name = "John Doe";
-        person1.age = 49;
+        person1.age = 46;
 //        System.out.println("Менья завут - " + person1.name + ", " + "Мну - " + person1.age);
 //        person1.speak();
         person1.sayHello();
@@ -15,6 +15,10 @@ public class ClassesAndObjects {
 //        System.out.println("Менья завут - " + person2.name + ", " + "Мну - " + person2.age);
 //        person2.speak();
         person2.speak();
+        int year1 = person1.calcYearToPen();
+        int year2 = person2.calcYearToPen();
+        System.out.println("Первому челу до пенсии: " + year1);
+        System.out.println("Второму челу до пенсии: " + year2);
     }
 }
 
@@ -25,12 +29,19 @@ class Person {
     String name;
     int age;
 
+    int calcYearToPen(){
+        int years = 65 - age;
+        return years;
+    }
+
     void speak() {
         for (int i = 0; i < 3; i++) {
             System.out.println("I'm " + name + " and " + age + " years old.");
-        }}
-    void sayHello () {
-    System.out.println("Hello");
         }
+    }
+
+    void sayHello() {
+        System.out.println("Hello");
+    }
 
 }
