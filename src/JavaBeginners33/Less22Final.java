e1b3f43c4cf71dd2cf115b064bd3c5ff33bb1f78
azesm <azesm@yahoo.com>
2024-05-15 14:02:16 +0300

    Less22Final

---
+++ src/JavaBeginners33/Less22Final.java
@@ -0,0 +1,21 @@
+package JavaBeginners33;
+
+public class Less22Final {
+    public static void main(String[] args) {
+        final int X = 10;
+        System.out.println(X);
+    }
+}
+
+class Test {
+    public static final int CONSTANT = 10;
+
+//    public Test(int CONSTANT) {
+//        this.CONSTANT = CONSTANT;
+//    }
+//
+//    public void setCONSTANT(int x) {
+//        this.CONSTANT = x;
+//    }
+
+}
