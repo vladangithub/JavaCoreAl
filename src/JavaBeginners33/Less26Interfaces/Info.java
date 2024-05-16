e7cb2f020af561d82382f13445f27113b934a10f
azesm <azesm@yahoo.com>
2024-05-16 21:24:45 +0300

    Less26Interfaces15-40

---
+++ src/JavaBeginners33/Less26Interfaces/Info.java
@@ -0,0 +1,5 @@
+package JavaBeginners33.Less26Interfaces;
+
+public interface Info {
+    public void showInfo();
+}
---
+++ src/JavaBeginners33/Less26Interfaces/Test.java
@@ -0,0 +1,10 @@
+package JavaBeginners33.Less26Interfaces;
+
+public class Test {
+    public static void main(String[] args) {
+        Animal animal1 = new Animal(1);
+        Person person1 = new Person("Dodddd");
+//        animal1.showInfo();
+//        person1.showInfo();
+    }
+}
