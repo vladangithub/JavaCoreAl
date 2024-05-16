2a29096c489412d1b5a068a332039cb07e56396d
azesm <azesm@yahoo.com>
2024-05-16 21:59:08 +0300

    Less26InterfacesEnd

--- src/JavaBeginners33/Less26Interfaces/Test.java
+++ src/JavaBeginners33/Less26Interfaces/Test.java
@@ -2,9 +2,21 @@ package JavaBeginners33.Less26Interfaces;
 
 public class Test {
     public static void main(String[] args) {
-        Animal animal1 = new Animal(1);
-        Person person1 = new Person("Dodddd");
+//        Animal animal1 = new Animal(1);
+//        Person person1 = new Person("Dodddd");
 //        animal1.showInfo();
 //        person1.showInfo();
+//        Info info1 = new Animal(1);
+//        Info info2 = new Person("Bobbbbb");
+//        info1.showInfo();
+//        info2.showInfo();
+        Animal animal1 = new Animal(1);
+        Person person1 = new Person("Zhuzhikkk");
+        outputInfo(animal1);
+        outputInfo(person1);
+    }
+
+    public static void outputInfo(Info info) {
+        info.showInfo();
     }
 }
