2ff5cebb43ad5242f1f757864f38098de4fb241a
azesm <azesm@yahoo.com>
2024-05-28 08:29:17 +0300

    DMDEVjavaCore1HomeWork6

--- src/DMDEV/Recursion.java
+++ src/DMDEV/Recursion.java
@@ -1,4 +1,19 @@
 package DMDEV;
 
+/* написать факториал введенного целого числа
+ * */
+
 public class Recursion {
+    public static void main(String[] args) {
+//          1 * 2 * 3 * 4 * 5 * 6 = 6!
+        int factorial = factorial(5);
+        System.out.println(factorial);
+    }
+
+    public static int factorial(int value) {
+        if (value == 1) {
+            return 1;
+        }
+        return value * factorial(value - 1);
+    }
 }
